26767c263c390efc973e51882067ea1ee689772b
Shizhan <68489799+LLmaomao2020@users.noreply.github.com>
2020-11-29 18:44:14 +0800

    Update LeapYear.java

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -2,42 +2,31 @@
  *  @author YOUR NAME HERE
  */
 public class LeapYear {
+	
+	public static boolean isLeapYear(int year){
+		if (year%400==0) { return true;}
+		else if (year%4==0 & year%100!=0) {return true;}
+		else {return false;}
+	}
+	
+	public static void checkLeapYear(int year) {
+		if(isLeapYear(year)) {System.out.printf("%d is a leap year. \n", year);}
+		else {System.out.printf("%d is not a leap year. \n", year);}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     for(int i=0;i<args.length;i++) {
+    	 
+    	 try {
+    		 int year=Integer.parseInt(args[i]);
+    		 checkLeapYear(year);
+    		 
+    	 }
+    	 catch (NumberFormatException e) {
+    		 System.out.printf("%s is not a valid number.\n",args[i]);
+    	 }
+     }
+	}
 
-    /** Calls isLeapYear to print correct statement.
-     *  @param  year to be analyzed
-     */
-    private static void checkLeapYear(int year) {
-        if (isLeapYear(year)) {
-            System.out.printf("%d is a leap year.\n", year);
-        } else {
-            System.out.printf("%d is not a leap year.\n", year);
-        }
-    }
-
-    private static boolean isLeapYear(int year){
-	    boolean result=false;
-	    if (year&400==0){
-		    result=true;
-	    }else if (year%4==0 & year%100!=0){
-		    result=true;
-	    }
-	    return result;
-    }
-
-    /** Must be provided an integer as a command line argument ARGS. */
-    public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Please enter command line arguments.");
-            System.out.println("e.g. java Year 2000");
-        }
-        for (int i = 0; i < args.length; i++) {
-            try {
-                int year = Integer.parseInt(args[i]);
-                checkLeapYear(year);
-            } catch (NumberFormatException e) {
-                System.out.printf("%s is not a valid number.\n", args[i]);
-            }
-        }
-    }
 }
-
